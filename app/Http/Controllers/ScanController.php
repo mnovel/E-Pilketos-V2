977f369dbc196a21e257fc3a7f08a938c590a79f
Muhammad Novel <66077593+mnovel@users.noreply.github.com>
2025-10-10 21:13:52 +0700

    Update ScanController.php

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -50,15 +50,11 @@ class ScanController extends Controller
             return $this->errorResponse(null, 'Participant already checked in');
         }
 
-        $electionSession = $participant->class->electionSession->first();
+        $electionSession = $participant->class->electionSession;
 
         $now = now();
         if ($now->lt($electionSession->start_date) || $now->gt($electionSession->end_date)) {
-            return $this->errorResponse([
-        'now' => $now->toDateTimeString(),
-        'start' => $electionSession->start_date->toDateTimeString(),
-        'end' => $electionSession->end_date->toDateTimeString(),
-    ], 'Not within election session time');
+            return $this->errorResponse(null, 'Not within election session time');
         }
 
         $participant->update([
