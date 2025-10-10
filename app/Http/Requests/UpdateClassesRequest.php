889983c898662956fd11110dd3fb3028aba01e38
Muhammad Novel <66077593+mnovel@users.noreply.github.com>
2025-10-10 20:08:14 +0700

    Update UpdateClassesRequest.php

--- app/Http/Requests/UpdateClassesRequest.php
+++ app/Http/Requests/UpdateClassesRequest.php
@@ -23,7 +23,7 @@ class UpdateClassesRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255|unique:classes,name,' . $this->route('class')->id,
-            'max_users' => 'required|integer|min:1|max:50',
+            'max_users' => 'required|integer|min:1|max:70',
             'election_session' => 'required|uuid|exists:election_sessions,id',
         ];
     }
