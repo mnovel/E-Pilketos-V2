e42e479e2d2e96a701e0839897d1f799b6913cab
Muhammad Novel <66077593+mnovel@users.noreply.github.com>
2025-10-11 06:25:37 +0700

    Update QuickCount.php

--- app/Http/Controllers/QuickCount.php
+++ app/Http/Controllers/QuickCount.php
@@ -15,32 +15,20 @@ class QuickCount extends Controller
 
     public function AllCount()
     {
-        $vote = Candidates::withCount('votes')
+        $candidates = Candidates::withCount('votes')
             ->orderBy('order_number')
             ->get();
-
-        $totalVotes = $vote->sum('votes_count');
-
-        $vote = $vote->map(function ($item) use ($totalVotes) {
-            $item->percentage = $totalVotes > 0
-                ? round(($item->votes_count / $totalVotes) * 100, 2)
-                : 0;
-
-            return (object) [
-                'candidate_id' => $item->id,
-                'name'         => $item->name,
-                'photo'        => $item->photo ? asset('storage/'.$item->photo) : '',
-                'order_number' => $item->order_number,
-                'total_votes'  => $item->votes_count,
-                'percentage'   => $item->percentage,
-            ];
-        });
-
+    
+        $totalVotes = $candidates->sum('votes_count');
+    
+        request()->merge(['total_votes' => $totalVotes]);
+    
         return $this->successResponse(
-            CandidateVoteResource::collection($vote),
+            CandidateVoteResource::collection($candidates),
             'Quick count retrieved successfully'
         );
     }
+
 
     public function CountByClass()
     {
