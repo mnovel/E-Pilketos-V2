c78aef5f630cf2492fc65e823f1c765825c84579
Muhammad Novel <66077593+mnovel@users.noreply.github.com>
2025-10-11 06:16:25 +0700

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,7 +3,6 @@ use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-    protected $proxies = '*'; // Atau bisa pakai array IP Cloudflare
-
+    protected $proxies = '*';
     protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
