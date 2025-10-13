a11d64be82f002420dbd10530ed815a7725c584a
Muhammad Novel <mnovel78@gmail.com>
2025-10-13 09:10:09 +0700

    tes

--- app/Models/Participants.php
+++ app/Models/Participants.php
@@ -33,7 +33,7 @@ class Participants extends Model
 
     public function BarcodeBallotBox()
     {
-        return $this->hasMany(BarcodeBallotBox::class);
+        return $this->hasMany(BarcodeBallotBox::class, 'participant_id');
     }
 
     public function votes()
